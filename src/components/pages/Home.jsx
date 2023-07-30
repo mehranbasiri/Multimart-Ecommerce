ce2ca0650454c57bca5db02b1731ab8208fa39bc
Mehran Basiri <Mehranbasirideveloper@gmail.com>
2023-07-30 15:10:14 +0330

    complete productlist section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Helmett from "../Helmet/Helmett";
 import "../../styles/Home.css";
 import { Container, Row, Col } from "reactstrap";
@@ -6,8 +6,25 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import heroImg from "../../assets/images/hero-img.png";
 import Services from "../../services/Services";
+import ProductList from "../UI/ProductList";
+import products from "../../assets/data/products";
 const Home = () => {
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  const [bestSalesProducts, setBestSalesProducts] = useState([]);
   const year = new Date().getFullYear();
+
+  useEffect(() => {
+    const filterdTrendingProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+
+    const filterdBestSalesProducts = products.filter(
+      (item) => item.category === "sofa"
+    );
+    setTrendingProducts(filterdTrendingProducts);
+    setBestSalesProducts(filterdBestSalesProducts);
+  }, []);
+
   return (
     <Helmett title={"Home"}>
       <section className="hero__section">
@@ -39,6 +56,26 @@ const Home = () => {
         </Container>
       </section>
       <Services />
+      <section className="trending__products">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Trending Products</h2>
+            </Col>
+            <ProductList data={trendingProducts} />
+          </Row>
+        </Container>
+      </section>
+      <section className="best__sales">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Best Sales</h2>
+            </Col>
+            <ProductList data={bestSalesProducts} />
+          </Row>
+        </Container>
+      </section>
     </Helmett>
   );
 };
--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./Services.css";
 import { Container, Row, Col } from "reactstrap";
 import serviceData from "../assets/data/serviceData";
+import { motion } from "framer-motion";
 const Services = () => {
   return (
     <Container>
       <Row>
         {serviceData.map((item, index) => (
           <Col lg="3" md="4" key={index}>
-            <div className="service__item" style={{ background: `${item.bg}` }}>
+            <motion.div whileHover={{scale:1.1}} className="service__item" style={{ background: `${item.bg}` }}>
               <span>
                 <i class={item.icon}></i>
               </span>
@@ -17,7 +18,7 @@ const Services = () => {
                 <h3>{item.title}</h3>
                 <p>{item.subtitle}</p>
               </div>
-            </div>
+            </motion.div>
           </Col>
         ))}
       </Row>
